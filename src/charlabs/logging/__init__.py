67cce24bf4a0961ed8284cdd6cf69e60f386f2b3
Charles-François St-Cyr <charlesfrancois.stcyr@moov.ai>
2025-06-27 08:12:21 -0400

    feat: remove unused structlog import from logging module

--- src/charlabs/logging/__init__.py
+++ src/charlabs/logging/__init__.py
@@ -1,10 +1,8 @@
-from charlabs.logging import structlog
 from charlabs.logging.default import LogsSettings, TaskLogger, setup_logs
 
 __version__ = "0.2.0"
 
 __all__ = [
-    "structlog",
     "LogsSettings",
     "setup_logs",
     "TaskLogger",
